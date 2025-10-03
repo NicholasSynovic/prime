ff6b7edd0342f17b2b84f41321b2895ee5d9c7cc
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-10-03 10:32:32 -0500

    Start work on a class to log events

---
+++ prime/api/logger.py
@@ -0,0 +1,31 @@
+import logging
+from logging import Logger
+from pathlib import Path
+from time import time
+
+
+class PRIME_Logger:
+    def __init__(self, log_file: Path, name: str) -> None:
+        # Variables to store timing information
+        self.time: float = 0
+
+        # Logger attributes
+        self.logger_name: str = name.lower()
+        self.log_file: Path = log_file.resolve()
+        self.logger: Logger = logging.getLogger(name=self.logger_name)
+
+        # Logger config
+        logging.basicConfig(
+            filename=self.log_file,
+            encoding="utf-8",
+            level=logging.DEBUG,
+        )
+
+    def start_timing(self, message: str) -> None:
+        self.time = time()
+        formatted_message: str = f"{message}: {self.time}"
+        logging.info(msg=formatted_message)
+
+    def end_timing(self, message: str) -> None:
+        formatted_message: str = f"{message}: {time() - self.time} seconds"
+        logging.info(msg=formatted_message)
